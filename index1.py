e093e5d481665729a3db6275a3a8eb296278f750
KODIVENKATESH <93427365+KODIVENKATESH@users.noreply.github.com>
2022-06-11 22:51:27 +0530

    Add files via upload

---
+++ index1.py
@@ -0,0 +1,5 @@
+l=['Gfg','is','best','for','Geeks']
+print("the required list is",l)
+#replacing g with e
+j=[n.replace("G","_").replace("e","G").replace("_","e") for n in l]
+print("after replacing",j)
